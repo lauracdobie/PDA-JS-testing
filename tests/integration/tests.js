f413234285bf02248433b7148b9ce0a254bc7d67
Laura Dobie <moving.toyshop@gmail.com>
2021-01-22 15:23:11 +0000

    Can add two numbers together

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -16,4 +16,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('should be able to add two numbers together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('5')
+  })
+
 });
