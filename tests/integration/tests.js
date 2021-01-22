057e07b5f548c8c3c365f8b04f221ef1968290b2
Laura Dobie <moving.toyshop@gmail.com>
2021-01-22 16:15:56 +0000

    UI integration test multiple operations chained together passes

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -25,4 +25,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('5')
   })
 
+  it('should be able to chain multiple operations together', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
+
 });
