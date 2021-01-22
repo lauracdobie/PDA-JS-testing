4539ac8552673a97f7e8ecacc67b834707e6cd64
Laura Dobie <moving.toyshop@gmail.com>
2021-01-22 16:29:28 +0000

    UI integration test diplays correct output for a range of numbers.

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -36,4 +36,51 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4')
   })
 
+  it('should be able to display the output for a positive number', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('40')
+  })
+
+  it('should be able to display the output for a negative number', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-14')
+  })
+
+  it('should be able to display the output for a decimal', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number7')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1.4')
+  })
+
+
+  it('should be able to display the output for a very large number', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number5')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8400000')
+  })
+
+
 });
